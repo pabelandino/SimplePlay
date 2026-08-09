089b2c5c7d3838518cb257472a45cac6206d15f1
Pabel <pabelandino@gmail.com>
2026-08-08 22:05:25 -0600

    Initial Commit

---
+++ SimplePlayTests/SimplePlayTests.swift
@@ -0,0 +1,18 @@
+//
+//  SimplePlayTests.swift
+//  SimplePlayTests
+//
+//  Created by Pabel Andino on 8/8/26.
+//
+
+import Testing
+
+struct SimplePlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
